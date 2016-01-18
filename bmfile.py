93fe564c482efc8057d9bda646e0182b28434a94
William Kupersanin <wkupersa@gmail.com>
2016-01-18 00:42:23 -0500

    added argument parsing

--- bmfile.py
+++ bmfile.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 import sys
 import getopt
@@ -5,35 +7,49 @@ import getopt
 class BookMarkFile(object):
 
 
-    def __init__(self, fileName):
+    def __init__(self, filename):
         self.bm_file = None
+        self.log_file = sys.stderr
 
+        print filename
         try:
-            self.bm_file=open(fileName,"r")
+            self.bm_file=open(filename,"r")
 
         except Exception as e:
-            print("Can't open bookmark file %s" % str(e), file=sys.stderr)
+            self.log_file.write("Can't open bookmark file %s\n" % str(e))
             sys.exit(-1)
 
-        while bm_file:
-            print _
+        for line in  self.bm_file:
+            print line
 
 
+def usage():
+    usage = """
+    -f, --filename\tfilename of bookmark file
+    -h, --help\t\tthis help information
+    """
+    print usage
+    sys.exit(0)
 
-def main():
+def main(argv):
+    
     try:
-        options, args = getopt.getopt(sys.argv[1:],"fh", ["file-name","help"])
+        options, args = getopt.getopt(argv,"f:h", ["filename=","help"])
     except getopt.GetoptError as e:
-            print("Option not recognized %s" % str(e), file=sys.stderr)
-            sys.exit(-1)
-
-    for o,a in opts:
-        print ("%s = %s" % (str(o),str(a)))
+            sys.stderr.write("Option not recognized %s\n" % str(e))
+            usage()
             
-
+    for o,a in options:
+        if o in ("-f", "--filename"):
+            filename = a
+        if o in ("-h", "--help"):
+            usage()
+        
+    bmf = BookMarkFile(filename)    
+    
 
     
             
 if __name__  == "__main__":
-    main()
+    main(sys.argv[1:])
         
